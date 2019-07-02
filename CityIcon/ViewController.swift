f3b95ca35c10b33776c63582359f407c01dd7dfc
erimsengezer <erimsengezer@gmail.com>
2019-07-02 17:26:03 +0300

    Initial Commit

---
+++ CityIcon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityIcon
+//
+//  Created by Erim Şengezer on 2.07.2019.
+//  Copyright © 2019 Erim Şengezer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
