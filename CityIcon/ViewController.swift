7023b64465f46c8b3679c41460f47adf2cf1b610
erimsengezer <erimsengezer@gmail.com>
2019-07-02 17:48:47 +0300

    Add Arrays & Add delete

--- CityIcon/ViewController.swift
+++ CityIcon/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
   
-    
+    var landmarkNames = [String]()
+    var landmarkImage = [UIImage]()
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
@@ -19,17 +20,39 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
         tableView.dataSource = self
         tableView.delegate = self
         
+        landmarkNames.append("Colleseum")
+        landmarkNames.append("Great Wall")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Stonehenge")
+        landmarkNames.append("Tac Mahal")
+        
+        landmarkImage.append(UIImage(named: "collesium.jpg")!)
+        landmarkImage.append(UIImage(named: "greatWall.jpg")!)
+        landmarkImage.append(UIImage(named: "kremlin.jpg")!)
+        landmarkImage.append(UIImage(named: "stonehenge.jpg")!)
+        landmarkImage.append(UIImage(named: "tacmahal.jpg")!)
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return landmarkNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            landmarkNames.remove(at: indexPath.row)
+            landmarkImage.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+            
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Landmarrk"
+        cell.textLabel?.text = landmarkNames[indexPath.row]
         return cell
-        
     }
 
 
